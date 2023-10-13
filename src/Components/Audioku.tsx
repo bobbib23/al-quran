85c570489431813ab60f5c18ea832609fdaf2899
bobbib <bobbib238@gmail.com>
2023-10-13 15:49:06 +0700

    pindah branch

--- src/Components/Audioku.tsx
+++ src/Components/Audioku.tsx
@@ -1,6 +1,6 @@
 function Audioku({ audi }: any) {
   return (
-    <div style={{ textAlign: "center" }}>
+    <div style={{ textAlign: "center", margin: "20px" }}>
       <audio controls src={audi["05"]} />
     </div>
   );
